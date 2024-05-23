49a9ddf31bef72d423e17107f0da45278f179eef
GavalaN <gavalan@kkszki.hu>
2024-05-23 14:26:33 +0200

    mentes

--- mai2/szallas.js
+++ mai2/szallas.js
@@ -10,10 +10,10 @@ function fetchGET(){
             ` <div class="card" style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">${adat.name}</h5>
-              <a  target="blank" href="www.${adat.hostname}">Weblap</a>
-              <p>${adat.location}</p>
-              <p>${adat.price} Ft</p>
-              <p>Minimum éjszakák száma ${adat.minimum_nights}</p> 
+              <a  target="blank" href="www.${adat.hostname}">Szállás weboldala</a>
+              <p>Hely: ${adat.location}</p>
+              <p>Ár: ${adat.price} Ft</p>
+              <p>Minimum éjszakák száma: ${adat.minimum_nights}</p> 
               <button id="torles" onclick="Torles(${adat.id})" class="btn btn-primary"> Törlés</button>
             </div>`;
         }
