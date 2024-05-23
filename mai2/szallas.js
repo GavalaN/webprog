cbaf7778f77859291ad78fee60afc154125e5efd
GavalaN <gavalan@kkszki.hu>
2024-05-23 13:39:13 +0200

    js

---
+++ mai2/szallas.js
@@ -0,0 +1,9 @@
+fetchGET(){
+    fetch('https://nodejs.sulla.hu/data')
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(adatok){
+        console.log(adatok)
+    })
+}
