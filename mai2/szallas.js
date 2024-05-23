c5768c35286507a7068ad089f1fe50da52634e2b
GavalaN <gavalan@kkszki.hu>
2024-05-23 14:26:17 +0200

    mentes

--- mai2/szallas.js
+++ mai2/szallas.js
@@ -1,9 +1,22 @@
 function fetchGET(){
-    fetch('https://nodejs.sulla.hu/data')
-    .then(function(res){
-        return res.json()
+    fetch("http://nodejs.sulla.hu/data")
+    .then(function(valasz){
+        return valasz.json()
     })
-    .then(function(adatok){
-        console.log(adatok)
+    .then(function(valasz2){
+        for (const adat of valasz2) {
+            console.log(adat)
+            document.getElementById("adatok").innerHTML += 
+            ` <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${adat.name}</h5>
+              <a  target="blank" href="www.${adat.hostname}">Weblap</a>
+              <p>${adat.location}</p>
+              <p>${adat.price} Ft</p>
+              <p>Minimum éjszakák száma ${adat.minimum_nights}</p> 
+              <button id="torles" onclick="Torles(${adat.id})" class="btn btn-primary"> Törlés</button>
+            </div>`;
+        }
     })
 }
+fetchGET()
