5ab4a2702c1bdc1b6827e4dd587747101e563a9f
GavalaN <gavalan@kkszki.hu>
2024-06-03 21:34:33 +0200

    grid rendszer + css

--- mai2/szallas.js
+++ mai2/szallas.js
@@ -1,26 +1,52 @@
-function fetchGET(){
+function fetchGET() {
     fetch("http://nodejs.sulla.hu/data")
     .then(function(valasz){
         return valasz.json()
     })
     .then(function(valasz2){
         for (const adat of valasz2) {
-            console.log(adat)
-            document.getElementById("adatok").innerHTML += 
-            ` <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">${adat.name}</h5>
-              <a  target="blank" href="www.${adat.hostname}">Szállás weboldala</a>
-              <p>Hely: ${adat.location}</p>
-              <p>Ár: ${adat.price} Ft</p>
-              <p>Minimum éjszakák száma: ${adat.minimum_nights}</p> 
-              <button id="torles" onclick="Torles(${adat.id})" class="btn btn-primary"> Törlés</button>
-            </div>`;
+            console.log(adat);
+            const card = document.createElement('div');
+            card.classList.add('card', 'col-md-3');
+            card.innerHTML = `
+                <div class="card-details">
+                    <h5 class="text-title">${adat.name}</h5>
+                    <div id="demo${adat.id}" class="collapse">
+                        <a target="_blank" href="www.${adat.hostname}">Szállás weboldala</a>
+                        <p class="text-body">Hely: ${adat.location}</p>
+                        <p class="text-body">Ár: ${adat.price} Ft</p>
+                        <p class="text-body">Minimum éjszakák száma: ${adat.minimum_nights}</p> 
+                    </div>
+                </div>
+                <div class="card-footer d-flex justify-content-between">
+                    <button class="btn" alt="Információk" type="button" onclick="toggleCollapse(${adat.id})">
+                        <i class="bi bi-info-circle"></i>
+                    </button>
+                    <button class="btn" alt="Módosítás" onclick="FetchPUT(${adat.id})">
+                        <i class="bi bi-wrench"></i>
+                    </button>
+                    <button class="btn" alt="Törlés" onclick="FetchDELETE(${adat.id})">
+                        <i class="bi bi-trash"></i>
+                    </button>
+                </div>
+            `;
+            document.getElementById('adatok').appendChild(card);
         }
-    })
+    });
 }
-fetchGET()
 
-for (const item of object) {
+function toggleCollapse(id) {
+    const demo = document.getElementById(`demo${id}`);
+    const isCollapsed = demo.classList.contains('show');
+    if (isCollapsed) {
+        demo.classList.remove('show');
+    } else {
+        demo.classList.add('show');
+    }
+}
+
+fetchGET();
+
+function fetchPOST() {
     
 }
